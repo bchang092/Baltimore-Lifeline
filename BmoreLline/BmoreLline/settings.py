2b34cb5a9f56be0eeb9778b4bd4d488e811c20f5
Brandon Chang <brandonchang@MacBook-Pro-4.local>
2025-09-18 00:41:28 -0400

    settings changed

--- BmoreLline/BmoreLline/settings.py
+++ BmoreLline/BmoreLline/settings.py
@@ -1,149 +1,82 @@
 from pathlib import Path
 import os
-import django_heroku
 import dj_database_url
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6x-@=dgg)wxl*38g@b+f26@9*)#d0+-kgp_-8sr!2ivyw42y9v'
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-
-
-
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-insecure-key")  # set on Heroku!
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
-
-
-
-# Application definition
-
-
 INSTALLED_APPS = [
-   'django.contrib.admin',
-   'django.contrib.auth',
-   'django.contrib.contenttypes',
-   'django.contrib.sessions',
-   'django.contrib.messages',
-   'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # your apps here, e.g. 'maps', etc.
 ]
-DEBUG = False
-
 
 MIDDLEWARE = [
-   'django.middleware.security.SecurityMiddleware',
-   'django.contrib.sessions.middleware.SessionMiddleware',
-   'django.middleware.common.CommonMiddleware',
-   'django.middleware.csrf.CsrfViewMiddleware',
-   'django.contrib.auth.middleware.AuthenticationMiddleware',
-   'django.contrib.messages.middleware.MessageMiddleware',
-   'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    # WhiteNoise for static files
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 
 ROOT_URLCONF = 'BmoreLline.urls'
 
-
 TEMPLATES = [
-   {
-       'BACKEND': 'django.template.backends.django.DjangoTemplates',
-       'DIRS': [os.path.join(BASE_DIR,'templates')],
-       'APP_DIRS': True,
-       'OPTIONS': {
-           'context_processors': [
-               'django.template.context_processors.request',
-               'django.contrib.auth.context_processors.auth',
-               'django.contrib.messages.context_processors.messages',
-           ],
-       },
-   },
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
-
 
 WSGI_APPLICATION = 'BmoreLline.wsgi.application'
 
-
-
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-
+# Database: prefer DATABASE_URL on Heroku, fall back to sqlite locally
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True,
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
-
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
-
-AUTH_PASSWORD_VALIDATORS = [
-   {
-       'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-   },
-   {
-       'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-   },
-   {
-       'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-   },
-   {
-       'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-   },
-]
-
-
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
-
 LANGUAGE_CODE = 'en-us'
-
-
 TIME_ZONE = 'UTC'
-
-
 USE_I18N = True
-
-
 USE_TZ = True
 
+# Static files
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'               # <-- fixed typo
+STATICFILES_DIRS = [BASE_DIR / 'static']             # if you have a /static directory
 
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'statifiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
-django_heroku.settings(locals())
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
+# WhiteNoise storage (Django 4/5+)
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
